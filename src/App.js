51e508ff6f4b49d2d5422e9eae6206e0163a0e75
kuldeep1972 <kuldeepgupta1971@gmail.com>
2023-08-24 00:08:12 +0530

    Learning about states

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
 
 function App() {
   return (
     <>
-      <Navbar title="TextUtils"/>
       {/* <Navbar title="TextUtils" aboutText="About TextUtils"/> */}
       {/* <Navbar/> */}
+      <Navbar title="TextUtils" />
+      <div className="container">
+        <TextForm heading="Converter"/>
+      </div>
     </>
   );
 }
